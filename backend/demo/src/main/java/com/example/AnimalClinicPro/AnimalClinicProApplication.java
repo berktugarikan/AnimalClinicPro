07870f57b6041ba8b6da19933e46183a48afb035
AlicanDagli <alicandgli@gmail.com>
2023-12-23 16:41:34 +0300

    start.spring

---
+++ backend/demo/src/main/java/com/example/AnimalClinicPro/AnimalClinicProApplication.java
@@ -0,0 +1,13 @@
+package com.example.AnimalClinicPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalClinicProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalClinicProApplication.class, args);
+	}
+
+}
---
+++ backend/demo/src/test/java/com/example/AnimalClinicPro/AnimalClinicProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AnimalClinicPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalClinicProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
