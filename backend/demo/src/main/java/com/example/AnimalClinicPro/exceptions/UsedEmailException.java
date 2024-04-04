1639902223eb1635c9fddc245402ca4427d90271
AlicanDagli <149877634+AlicanDagli@users.noreply.github.com>
2024-04-05 01:52:11 +0300

    w/Berktuğ file correction

--- backend/demo/src/main/java/com/example/AnimalClinicPro/exceptions/UsedEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.example.AnimalClinicPro.exceptions;
-
-public class UsedEmailException extends Exception{
-    public UsedEmailException(String message){
-        super(message);
-    }
-}
